0f27057336f48c082931ddb0d14867e90b4f894f
Edgar Parra <edgar@alteracorp.cl>
2026-01-22 17:24:59 -0300

    Add migrations & command & model ApiClient

---
+++ app/Console/Commands/AddAPIClient.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\ApiClient;
+use Illuminate\Console\Command;
+
+class AddAPIClient extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api-client:add {name} {api-key-id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a new API Client';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+
+        $apiKeyId = $this->argument('api-key-id');
+
+        if (ApiClient::where('api_key_id', $apiKeyId)->exists()) {
+
+            $this->error("El ID de la API Key ya existe");
+
+            exit;
+        }
+
+        $apiKey = bin2hex(random_bytes(32));
+
+        $secretKey = bin2hex(random_bytes(32));
+
+        ApiClient::create([
+            'name' => $name,
+            'api_key_id' => $apiKeyId,
+            'api_key_hash' => $apiKey,
+            'api_secret' => $secretKey,
+        ]);
+
+        $this->info("API Key: {$apiKey}");
+
+        $this->info("Secret Key: {$secretKey}");
+    }
+}
---
+++ app/Models/ApiClient.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApiClient extends Model
+{
+     protected $fillable = [
+        'name',
+        'api_key_id',
+        'api_key_hash',
+        'api_secret',
+        'rate_limit',
+        'is_active',
+        'revoked_at',
+    ];
+
+    protected $hidden = [
+        'api_key_hash',
+        'api_secret',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'api_key_hash' => 'hashed',
+            'is_active' => 'boolean',
+            'last_used_at' => 'datetime',
+            'revoked_at' => 'datetime',
+        ];
+    }
+}
+
+
