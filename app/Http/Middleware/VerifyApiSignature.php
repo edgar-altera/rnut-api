1b8d3dea0dbf46abc8ddea8dd1d14b8b758ee0c0
Edgar Parra <edgar@alteracorp.cl>
2026-01-28 13:11:33 -0300

    Add VerifyApiSignature middleware

---
+++ app/Http/Middleware/VerifyApiSignature.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class VerifyApiSignature
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $signature = $request->header('x-signature');
+        
+        $timestamp = $request->header('x-timestamp');
+
+        if (! $signature || ! $timestamp) {
+
+            throw new AccessDeniedHttpException(__('messages.signature_missing'));
+        }
+
+        if (abs(time() - (int) $timestamp) > 300) {
+
+            throw new AccessDeniedHttpException(__('messages.signature_expired'));
+        }
+
+        $apiClient = $request->attributes->get('apiClient');
+
+        if (! $apiClient) {
+
+            throw new AccessDeniedHttpException(__('messages.api_key_invalid'));
+        }
+
+        $payload = implode("\n", [
+            $request->method(),
+            $request->getPathInfo(),
+            $request->getQueryString() ?? '',
+            $timestamp,
+        ]);
+
+        $expectedSignature = hash_hmac(
+            'sha256',
+            $payload,
+            $apiClient->api_secret
+        );
+
+        if (! hash_equals($expectedSignature, $signature)) {
+            
+            throw new AccessDeniedHttpException(__('messages.signature_invalid'));
+        }
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use App\Http\Middleware\EnsureJsonRequest;
 use App\Http\Middleware\RateLimit;
 use App\Http\Middleware\SetLang;
 use App\Http\Middleware\ValidateApiKey;
+use App\Http\Middleware\VerifyApiSignature;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -20,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         
         $middleware->append(CheckApiIp::class);
         $middleware->append(ValidateApiKey::class);
+        $middleware->append(VerifyApiSignature::class);
         $middleware->append(AddContext::class);
         $middleware->append(SetLang::class);
         $middleware->append(EnsureJsonRequest::class);
