e35a8098b29569ba2830407b5f333eca40cf2f71
Edgar Parra <edgar@alteracorp.cl>
2026-02-04 14:01:19 -0300

    Excluded middlewares - health endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,22 @@
 
 use App\Http\Controllers\HealthController;
 use App\Http\Controllers\VehicleController;
+use App\Http\Middleware\RateLimit;
+use App\Http\Middleware\ValidateApiKey;
+use App\Http\Middleware\ValidateClientIpMiddleware;
+use App\Http\Middleware\ValidateNonce;
+use App\Http\Middleware\ValidateApiSignature;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     
-    Route::get('/health', HealthController::class);
+    Route::get('/health', HealthController::class)->withoutMiddleware([
+        RateLimit::class,
+        ValidateApiKey::class,
+        ValidateClientIpMiddleware::class,
+        ValidateNonce::class,
+        ValidateApiSignature::class,
+    ]);
 
     Route::get('/vehicles/{licensePlate}', [VehicleController::class, 'show']);
 
